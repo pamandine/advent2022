508ac8ef3028316f58c2b05604bd9c80d0864cab
Amandine P <amandine.poterala@gmail.com>
2022-12-12 20:02:43 +0100

    MAJ decembre01&02

--- decembre01/decembre01.py
+++ decembre01/decembre01.py
@@ -1,6 +1,5 @@
 resume = {}
 sum = 0
-max = 0
 current = 0
 
 f = open("decembre01.txt", "r")
@@ -8,27 +7,22 @@ for line in f.readlines():
     line = line.rstrip("\n")
     if (line == ""): # New one
         resume[current] = sum
-        if (sum > max):
-            max = sum
 
         sum = 0
         current = current+1
     else:    
         sum = sum + int(line);
+f.close()
 
 # Sort by value in increasing order
 sorted = {k: v for k, v in sorted(resume.items(), key=lambda item: item[1])}
 
 # Get the 3 big, and add them
 sorted_keys = list(sorted)
-print(sorted_keys)
-
-size = len(sorted_keys)-1
 sum = 0
-for i in range(size,size-3,-1):
+for i in [-1,-2,-3]:
     sum = sum + sorted[sorted_keys[i]]
 
-biggerOfAll = sorted[sorted_keys[size]]
+biggerOfAll = sorted[sorted_keys[-1]]
 print (f'[PART1] Sum of 1 bigger = {biggerOfAll}')
 print (f'[PART2] Sum of 3 bigger = {sum}')
-f.close()
---
+++ decembre02/decembre02.py
@@ -0,0 +1,101 @@
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+
+WON = 6
+DRAW = 3
+LOSS = 0
+
+opponent = {
+        "A" : "R", # Rock
+        "B" : "P", # Paper
+        "C" : "S" # Scissors
+        }
+myself = {
+    "X" : "R", # Rock
+    "Y" : "P", # Papier
+    "Z" : "S" # Scissors
+        }
+predict = {
+    "X" : LOSS, 
+    "Y" : DRAW,
+    "Z" : WON
+        }
+expected = { # What should I have to lose, win or draw
+    DRAW : {
+        "P" : "P",
+        "S" : "S",
+        "R" : "R",
+        },
+    LOSS : {
+        "P" : "R",
+        "S" : "P", 
+        "R" : "S",
+        },
+    WON : {
+        "P" : "S", 
+        "R" : "P", 
+        "S" : "R",
+        },
+     }
+
+def whoWin(adv,me):
+    ret = DRAW # Null
+    if (adv == me):
+        ret = DRAW
+    elif ((adv == "R" and me == "P")
+          or (adv == "S" and me == "R")
+          or (adv == "P" and me == "S")):
+        ret = WON # me win
+    else:
+        ret = LOSS # me fail
+    return ret
+
+def whatShape(adv,result):
+    return expected[result][adv]
+
+def shapePoints(figure):
+    points = 0
+    if (figure == "R"):
+        points = ROCK
+    elif (figure == "S"):
+        points = SCISSORS
+    else:
+        points = PAPER
+    return points
+
+def func1(lines):
+    myPoints = 0
+    for line in lines:
+        line = line.rstrip("\n")
+        step = line.split()
+        adv = opponent[step[0]]
+        me = myself[step[1]]
+        winPoints = whoWin(adv, me)
+        figurePoints = shapePoints(me)
+        myPoints = myPoints + winPoints + figurePoints 
+        #print(f'adv={adv}, me={me}, winPts={winPoints}, shapePts={figurePoints}, sumPoints={myPoints}')
+
+    print(myPoints)
+
+def func2(lines):
+    myPoints = 0
+    for line in lines:
+        line = line.rstrip("\n")
+        step = line.split()
+        adv = opponent[step[0]] # Opponent shape
+        winPoints = predict[step[1]] # What I should do (DRAW, WIN, LOSS). These are directly the points
+        me = whatShape(adv,winPoints)
+        figurePoints = shapePoints(me)
+        myPoints = myPoints + winPoints + figurePoints 
+        #print(f'adv={adv}, me={me}, winPts={winPoints}, shapePts={figurePoints}, sumPoints={myPoints}')
+
+    print(myPoints)
+
+f = open("decembre02.txt", "r")
+lines = f.readlines()
+print('Call function 1')
+func1(lines)
+print('Call function 2')
+func2(lines)
+f.close()
