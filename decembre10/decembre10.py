4d6290521552ea9b894e65bab7283cbe7811416e
Amandine P <amandine.poterala@gmail.com>
2022-12-17 18:33:49 +0100

    Complete decembre10

--- decembre10/decembre10.py
+++ decembre10/decembre10.py
@@ -1,24 +1,31 @@
-debug=True
-if debug:
-	f = open("inputs/in10Example.txt", "r")
-else:
-	 f = open("inputs/in10.txt", "r")
+ f = open("inputs/in10.txt", "r")
+
+def manageCycle(curCycle,cycles,x,img):
+    inLine = (curCycle%40)
+    if (inLine==0):
+        img +="\n"
+    print(f'x={x}, inLine={inLine}')
+    if ((x-1)<=inLine and (x+1)>=inLine):
+        img += "#"
+    else:
+        img += "."
+    
+    curCycle += 1
+    cycles[curCycle] = x
+    return (curCycle,cycles,img)
 
 x = 1
-img=""
+txt=""
 cycles = {} # To save for debug
 curCycle = 0 # Number of current cycle
 for line in f.readlines():
     line = line.rstrip("\n")
     if (line[:4]=="noop"):
-        curCycle += 1
-        cycles[curCycle] = x        
+        (curCycle,cycles,txt) = manageCycle(curCycle,cycles,x,txt)
     else:
         (instruction,V) = line.split()
-        curCycle += 1
-        cycles[curCycle] = x        
-        curCycle += 1
-        cycles[curCycle] = x        
+        (curCycle,cycles,txt) = manageCycle(curCycle,cycles,x,txt)
+        (curCycle,cycles,txt) = manageCycle(curCycle,cycles,x,txt)
         x +=  int(V) # Result is computed at the end of the 2 cycles
 
 f.close()
@@ -32,3 +39,4 @@ for i in [20,60,100,140,180,220]:
     sum += strength[i]
 
 print(f'sum of 20,60,100,140,180 and 220 is {sum}')
+print(txt)
