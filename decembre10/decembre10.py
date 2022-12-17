56219d5d46588d167a6b293361416f21148afd39
Amandine P <amandine.poterala@gmail.com>
2022-12-17 18:39:31 +0100

    Improved dec10

--- decembre10/decembre10.py
+++ decembre10/decembre10.py
@@ -1,42 +1,33 @@
- f = open("inputs/in10.txt", "r")
-
-def manageCycle(curCycle,cycles,x,img):
-    inLine = (curCycle%40)
+def manageCycle(cycle,x,res,img):
+    inLine = (cycle%40)
     if (inLine==0):
         img +="\n"
-    print(f'x={x}, inLine={inLine}')
     if ((x-1)<=inLine and (x+1)>=inLine):
         img += "#"
     else:
         img += "."
     
-    curCycle += 1
-    cycles[curCycle] = x
-    return (curCycle,cycles,img)
+    cycle += 1
+    if cycle in [20,60,100,140,180,220]:
+        res += (cycle*x)
+    return (cycle,res,img)
 
 x = 1
 txt=""
-cycles = {} # To save for debug
-curCycle = 0 # Number of current cycle
+cycle = 0 # Number of current cycle
+res = 0
+f = open("inputs/in10.txt", "r")
 for line in f.readlines():
     line = line.rstrip("\n")
     if (line[:4]=="noop"):
-        (curCycle,cycles,txt) = manageCycle(curCycle,cycles,x,txt)
+        (cycle,res,txt) = manageCycle(cycle,x,res,txt)
     else:
         (instruction,V) = line.split()
-        (curCycle,cycles,txt) = manageCycle(curCycle,cycles,x,txt)
-        (curCycle,cycles,txt) = manageCycle(curCycle,cycles,x,txt)
+        (cycle,res,txt) = manageCycle(cycle,x,res,txt)
+        (cycle,res,txt) = manageCycle(cycle,x,res,txt)
         x +=  int(V) # Result is computed at the end of the 2 cycles
 
 f.close()
-strength = {}
-for e in cycles.keys():
-    strength[e] = e*cycles[e]
 
-
-sum = 0
-for i in [20,60,100,140,180,220]:
-    sum += strength[i]
-
-print(f'sum of 20,60,100,140,180 and 220 is {sum}')
+print(f'sum of 20,60,100,140,180 and 220 is {res}')
 print(txt)
